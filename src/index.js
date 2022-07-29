d531f6797407a1ba9921be897b5ad1ccddca2817
GabrielRuizVarela <gabrielruizvarela@gmail.com>
2022-07-29 14:14:50 -0300

    Fixed Images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.scss';
 import generateBars from './DOM';
 import logo from './logo.svg';
+import favicon from './favicon-32x32.png';
 
 generateBars(document.querySelector('#bars'));
-document.querySelector('img').src = logo;
